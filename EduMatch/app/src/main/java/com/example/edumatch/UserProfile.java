b28f5f9787b7584fca254e6b1e847f2e63d77833
Roberto-Osores <osoresroberto@gmail.com>
2023-10-03 07:32:08 -0300

    Arreglado error en boton home de activity UserProfile

--- EduMatch/app/src/main/java/com/example/edumatch/UserProfile.java
+++ EduMatch/app/src/main/java/com/example/edumatch/UserProfile.java
@@ -2,13 +2,29 @@ package com.example.edumatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserProfile extends AppCompatActivity {
+
+    Button btn_home;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        btn_home =findViewById(R.id.button_home);
+
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserProfile.this, HomePageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
