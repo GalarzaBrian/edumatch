68074f446d5f0b9061491cb922e9506103b48a4c
danit3x <dvargas087@gmail.com>
2023-10-29 23:32:13 -0300

    put and delete fixed

--- back-end/Edu-Match/src/main/java/com/edumatch/EduMatch/controller/UserController.java
+++ back-end/Edu-Match/src/main/java/com/edumatch/EduMatch/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.edumatch.EduMatch.controller;
 
+import com.edumatch.EduMatch.models.UserEntity;
 import com.edumatch.EduMatch.models.request.UserRequest;
 import com.edumatch.EduMatch.models.response.UserResponse;
+import com.edumatch.EduMatch.repository.UserRepository;
 import com.edumatch.EduMatch.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,6 +23,7 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -25,10 +31,14 @@ public class UserController {
         return userService.findAllUsers();
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping()
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
+    public void deleteUser(@CurrentSecurityContext(expression = "authentication") Authentication authentication) {
+        String email = authentication.getName();
+        Optional<UserEntity> id = userRepository.findByEmail(email);
+        UserEntity foundUser = userRepository.findById(id.get().getId()).orElseThrow();
+
+        userService.deleteUser(foundUser.getId());
     }
 
 
@@ -39,9 +49,9 @@ public class UserController {
 
     }
 
-    @PutMapping("/{id}")
+    @PutMapping()
     @ResponseStatus(HttpStatus.OK)
-    public void updateUser(@Valid @RequestBody UserRequest request, @PathVariable Long id) {
-        userService.updateUser(request, id);
+    public void updateUser(@Valid @RequestBody UserRequest request) {
+        userService.updateUser(request);
     }
 }
